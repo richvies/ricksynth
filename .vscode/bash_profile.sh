332c58bf81d72f032e28abf26a61d5de74d922c5
Richard Davies <richvies@gmail.com>
2022-08-30 13:10:42 +0100

    bash profile: increase history size

--- .vscode/bash_profile.sh
+++ .vscode/bash_profile.sh
@@ -3,3 +3,6 @@ source $HOME/.bash_profile
 touch ".vscode/.bash_history"
 export HISTFILE=".vscode/.bash_history"
 export HISTCONTROL=ignoredups
+export HISTSIZE=10000
+export HISTFILESIZE=10000
+export HISTCONTROL=ignoredups
